6f18f071617be6b9b1f53900c9b17ac25ec8ac8b
haruki <kohara321tkj@icloud.com>
2019-06-16 05:54:50 +0000

    test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    @user = User.new(user_params)
+    if @user.save
       redirect_to root_path, success: 'ログインに成功しました'
     else
        flash.now[:danger] = 'ログインに失敗しました'
@@ -30,6 +29,6 @@ class SessionsController < ApplicationController
 end
 
   private
-  def user.params
+  def user_params
     params.require(:user).permit(:email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, length: {in: 8..32 }
-  validates :name, length: { maximum: 15}
-  _email = /\A\S+@\S+\.\S+\z/
+  validates :name, presence: true, length: {maximun: 15}
+  validates :email, presence: true, format:{with: /\A\S+@\S+\.\S+\z/ }
+  validates :password, length: {in: 8..32 }, format:{with: /\A[a-zA-Z0-9]+\z/ }
   
-  _password = /\A[a-zA-Z0-9]+\z/
 
   has_secure_password
   
   
-  has_many :topics
+  has_many : topics
 end
