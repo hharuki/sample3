7099d39af3cc1ae8d4ded3382244011848becc2b
haruki <kohara321tkj@icloud.com>
2019-06-26 10:21:49 +0000

    modified

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  
+  root 'pages#index'
+  get 'pages/help'
+  
+  get  '/login',  to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  resources :favorites
+end
